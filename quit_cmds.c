c46a66b6cc1b85ae77579b6730cffd5f2508c2ce
shady-cj <petersp2000@gmail.com>
2022-08-11 08:54:39 +0100

    bug: fixing bugs

--- quit_cmds.c
+++ quit_cmds.c
@@ -1,5 +1,5 @@
 #include "main.h"
-
+int check_alpha(char *);
 /**
  * handle_exit - A simple helper function that exits the shell when exit
  * command is typed
@@ -9,6 +9,39 @@
 
 int handle_exit(char __attribute__((unused))**argv)
 {
+	if (argv[1] != NULL)
+	{
+		if (check_alpha(argv[1]))
+		{
+			fprintf(stderr, "./shell: 1: exit: ");
+			fprintf(stderr, "Illegal number: %s\n", argv[1]);
+			return (2);
+		}
+		if (atoi(argv[1]) < 0)
+		{
+			fprintf(stderr, "./shell: 1: exit: ");
+			fprintf(stderr, "Illegal number: %s\n", argv[1]);
+			return (2);
+		}
+	}
 	handle_EXIT(argv[1] ? atoi(argv[1]) : 0);
+	return (0);
+}
+/**
+ * check_alpha - This function checks a string and check if alphabet are
+ * present
+ * @str: The string
+ * Return: 1 if found 0 if not
+ */
+int check_alpha(char *str)
+{
+	int i = 0;
+
+	while (str[i])
+	{
+		if (str[i] > 57 || str[i] < 48)
+			return (1);
+		i++;
+	}
 	return (0);
 }
